7c636613275e02b4c0a01c8fafe4dcc874681dba
AbdulTheActivePiecer <abdulyki@activepieces.com>
2023-07-11 14:47:37 +0300

    chore: fixed navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,54 +1,111 @@
+'use client';
+
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { useRef } from "react";
 
 const Navbar = () => {
+  const routesList= useRef<null | HTMLDivElement>(null);
+  const buttonSvg= useRef<null | SVGSVGElement>(null);
+  const showNavbarOnMobile = ()=>{
+  routesList.current?.classList.toggle('top-[70px]');
+  buttonSvg.current?.classList.toggle('!stroke-green');
+};
   return (
-    <nav  className="bg-transparent sticky z-50 top-0 ">
-      <div className="bg-black w-full flex justify-center">
-      <div className="flex items-center justify-between flex-wrap p-6 bg-black container">
-            <div className="flex items-center flex-shrink-0 text-white mr-6">
+    <nav  className="bg-black sticky z-50 top-0 shadow-navbar h-[75px] md:h-[initial]  ">
+      
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4 z-20">
+        <div className="absolute top-0 left-0 bg-black md:static w-full md:w-[initial] flex justify-between items-center p-4 md:p-0 z-50">
         <Link href="/">
           <Image
             src="https://cdn.activepieces.com/brand/full-logo-white.svg"
             alt="Activepieces Logo"
-            width={150}
-            height={24}
+            width={215}
+            height={35}
+            className="w-[152px] h-[25px] md:w-[215px] md:h-[35px]"
           />
         </Link>
-      </div>
-
-      <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
-        <div className="text-sm lg:flex-grow"></div>
-        <div>
-          <Link
-            href="/blog"
-            className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-gray-400 mr-4 align-right"
-          >
-            Blog
-          </Link>
-          <Link
-            href="/pricing"
-            className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-gray-400 mr-4 align-right"
-          >
-            Pricing
-          </Link>
-          <Link
-            href="/pieces"
-            className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-gray-400 mr-4 align-right"
-          >
-            Pieces
-          </Link>
-          <Link
-            href="https://cloud.activepieces.com/"
-            className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-gray-400 mr-4 align-right"
-          >
-            Get Started
-          </Link>
+    <button data-collapse-toggle="navbar-default" type="button" onClick={showNavbarOnMobile} className="md:hidden inline-flex items-center p-2 w-10 justify-center
+     h-10  text-sm text-gray-500 rounded-lg" aria-controls="navbar-default" aria-expanded="false">
+        <span className="sr-only">Open main menu</span>
+        <svg ref={buttonSvg} className="w-5 h-5 stroke-grey " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15"/>
+        </svg>
+         </button>
         </div>
-      </div>
-      </div>
-      </div>  
+     
+
+    <div ref={routesList} className=" shadow-navbar md:shadow-none duration-[400ms]  z-10 w-full absolute -top-[225px] transition-all  left-0 bg-black md:static md:block md:w-auto" id="navbar-default">
+      <ul className="text-base flex flex-col gap-6 pl-6 md:pl-0 md:gap-0 p-4 md:p-0 md:flex-row md:space-x-8 md:mt-0">
+        <li className="flex order-4 md:order-none   justify-start md:justify-center items-center">
+       
+        <Link href="#" className="block gap-2 order-3 md:order-none  text-white hover:text-green flex  justify-start md:justify-center items-center transition-colors">
+          <Image
+          alt="Pieces" 
+          src="/pieces.png"
+          className="w-[20px] h-[20px] hidden md:block"
+          height={20}
+          width={20}>
+          </Image>
+            Pieces</Link>
+        </li>
+        <li className="flex order-5 md:order-none   justify-start md:justify-center items-center">
+       
+        <Link href="#" className="block gap-2 text-white hover:text-green flex  justify-start md:justify-center items-center transition-colors">
+          <Image
+          alt="Pieces" 
+          src="/pricing.png"
+          className="w-[20px] h-[20px] hidden md:block"
+          height={20}
+          width={20}>
+
+          </Image>
+            Pricing</Link>
+        </li>
+        <li className="flex items-center order-6 md:order-none ">
+        
+        <Link href="#" className="block gap-2 text-white hover:text-green flex  justify-start md:justify-center items-center transition-colors">
+          <Image
+          alt="Pieces" 
+          src="/docs.png"
+          className="w-[20px] h-[20px] hidden md:block"
+          height={20}
+          width={20}>
+          </Image>
+          Docs</Link>
+        </li>
+        <li className="flex items-center order-7 md:order-none ">
+       
+          <Link href="#" className="block gap-2 text-white hover:text-green flex  justify-start md:justify-center items-center transition-colors">
+          <Image
+          alt="Pieces" 
+          src="/contact.png"
+          className="w-[20px] h-[20px] hidden md:block"
+          height={20}
+          width={20}>
+          </Image>
+          Discord</Link>
+        </li>
+        <li className="flex items-center order-2 md:order-none">
+       
+       <Link href="#" className="block gap-2 text-white  flex bg-[black] md:hover:bg-[#433d51] justify-start md:justify-center items-center transition-colors md:px-3 md:py-3 rounded-lg  ">
+       Login</Link>
+     </li>
+        <li className="flex items-center md:!ml-2  order-1 md:order-none">
+       
+          <Link href="#" className="block gap-2 text-white  flex bg-[black] md:hover:bg-[#433d51] justify-start md:justify-center items-center transition-colors md:px-6 md:py-3 rounded-lg md:border md:border-white ">
+            <span className="hidden md:block">
+            Start free
+            </span>
+            <span className="block md:hidden">
+            Get started
+            </span>
+         </Link>
+        </li>
+ 
+      </ul>
+    </div>
+  </div>
     </nav>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -19,6 +19,7 @@ module.exports = {
       },
       colors: {
         primary: { DEFAULT: '#6e41e2', medium:'#ab90ee', light: '#eee9fc', dark: '#6838e0','10':'rgba(110, 65, 226, 0.1)' },
+        green: "#c9fa75",
         card: {
           DEFAULT: '#232429',
         },
@@ -37,15 +38,16 @@ module.exports = {
         yellow: {
           DEFAULT: '#FFCC4D'
         },
-        green: {
-          DEFAULT: '#06FFB4'
-        }
+        grey: '#6B7280'
       },
       fontFamily: {
         'sans': ['Eudoxus Sans', ...defaultTheme.fontFamily.sans],
       },
       fontSize: {
         '2sm': '1.25rem',
+      },
+      boxShadow: {
+        'navbar':'1px 1px 20px 1px #000'
       }
     },
   },
