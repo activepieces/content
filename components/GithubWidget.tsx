4445bc7f2339823f868584cb6a58f9762b4da77c
AbdulTheActivePiecer <abdulyki@activepieces.com>
2023-07-11 17:31:16 +0300

    chore: fetch real stars from repo

--- components/GithubWidget.tsx
+++ components/GithubWidget.tsx
@@ -2,14 +2,17 @@ import Link from "next/link";
 
 
 
-const GithubWidget = () => {
+const GithubWidget = async () => {
+  const repo = await fetch("https://api.github.com/repos/activepieces/activepieces");
+  const repoData = await repo.json();
+  const stars = repoData.stargazers_count;
   return (
     <div   className="border-1  border-solid border-[#ccd1d5] overflow-hidden rounded flex">
         <Link href="https://github.com/activepieces/activepieces" className="bg-[#ebf0f4] px-[10px] py-[5px] flex items-center " target="_blank"  rel="noopener noreferrer">
-        <svg viewBox="0 0 16 16" width="16" height="16" aria-hidden="true"><path d="M8 .25a.75.75 0 0 1 .673.418l1.882 3.815 4.21.612a.75.75 0 0 1 .416 1.279l-3.046 2.97.719 4.192a.751.751 0 0 1-1.088.791L8 12.347l-3.766 1.98a.75.75 0 0 1-1.088-.79l.72-4.194L.818 6.374a.75.75 0 0 1 .416-1.28l4.21-.611L7.327.668A.75.75 0 0 1 8 .25Zm0 2.445L6.615 5.5a.75.75 0 0 1-.564.41l-3.097.45 2.24 2.184a.75.75 0 0 1 .216.664l-.528 3.084 2.769-1.456a.75.75 0 0 1 .698 0l2.77 1.456-.53-3.084a.75.75 0 0 1 .216-.664l2.24-2.183-3.096-.45a.75.75 0 0 1-.564-.41L8 2.694Z"></path></svg>
+        <svg viewBox="0 0 16 16" width="16px" height="16px" aria-hidden="true"><path d="M8 .25a.75.75 0 0 1 .673.418l1.882 3.815 4.21.612a.75.75 0 0 1 .416 1.279l-3.046 2.97.719 4.192a.751.751 0 0 1-1.088.791L8 12.347l-3.766 1.98a.75.75 0 0 1-1.088-.79l.72-4.194L.818 6.374a.75.75 0 0 1 .416-1.28l4.21-.611L7.327.668A.75.75 0 0 1 8 .25Zm0 2.445L6.615 5.5a.75.75 0 0 1-.564.41l-3.097.45 2.24 2.184a.75.75 0 0 1 .216.664l-.528 3.084 2.769-1.456a.75.75 0 0 1 .698 0l2.77 1.456-.53-3.084a.75.75 0 0 1 .216-.664l2.24-2.183-3.096-.45a.75.75 0 0 1-.564-.41L8 2.694Z"></path></svg>
         </Link>
-        <Link href="https://github.com/activepieces/activepieces/stargazers" className="border-l-1  text-xs border-solid border-[#dededf]  px-[10px] py-[5px] bg-white text-black hover:text-[#0969da]" target="_blank"  rel="noopener noreferrer">
-              2,712
+        <Link href="https://github.com/activepieces/activepieces/stargazers" className="border-l  text-xs border-solid border-[#dededf]  px-[10px] py-[5px] bg-white text-black hover:text-[#0969da]" target="_blank"  rel="noopener noreferrer">
+              {Math.floor(stars/1000)},{stars%1000}
         </Link>
 
     </div>
