501062df3c291631b60e1fd816d1c2531ed6fec0
AbdulTheActivePiecer <abdulyki@activepieces.com>
2023-07-20 21:37:03 +0300

    chore: finished the four list sections

--- components/landing-page/CenterOfWorldCard.tsx
+++ components/landing-page/CenterOfWorldCard.tsx
@@ -1,17 +1,27 @@
 import Image from "next/image";
 import Link from "next/link";
 import { FlowTemplate } from '@activepieces/shared'
-export interface AiCardProps {
+export interface CenterOfWorldCard {
     template: FlowTemplate,
-    piecesIcons: string[]
+    piecesIcons: string[],
+    bannerUrl: string
 }
 
-const AiCard = (props: AiCardProps) => {
+const CenterOfWorldCard = (props: CenterOfWorldCard) => {
 
     return (
         <>
             <div className="border border-solid border-[#000000] rounded-[6px] overflow-hidden lg:min-w-[358px] min-w-[250px]" >
+                <div className={`w-full bg-cover bg-norepeat  h-[140px]`} style={{ backgroundImage: `url('${props.bannerUrl}')` }}></div>
                 <div className="p-[25px] lg:pb-[65px] lg:p-[35px] bg-white flex flex-col gap-[30px]">
+
+
+
+                    <div className="text-h6-sm lg:text-h6-lg text-black  leading:[21.6px] lg:leading-[31.9px] !font-bold  " >
+                        {
+                            props.template.description
+                        }
+                    </div>
                     <div className="flex gap-[10px] lg:gap-[15px]">
                         {
                             props.piecesIcons.map((icon, idx) => (
@@ -21,20 +31,16 @@ const AiCard = (props: AiCardProps) => {
                             ))
                         }
                     </div>
-                    <div className="text-h6-sm lg:text-h6-lg text-black  leading:[21.6px] lg:leading-[31.9px] !font-bold  " >
-                        {
-                            props.template.description
-                        }
-                    </div>
+
 
                 </div>
-                <Link target="_blank" rel="noopener noreferrer" className="bg-black text-white text-h4-sm lg:text-h4-lg  text-center py-[15px] lg:py-[10px]  flex justify-center" href={"https://cloud.activepieces.com/templates/" + props.template.id}>
-                    View automation
+                <Link target="_blank" rel="noopener noreferrer" className="bg-black text-white  justify-between text-h4-sm lg:text-h4-lg  text-center py-[15px] lg:py-[10px] px-[35px] items-center flex " href={"https://cloud.activepieces.com/templates/" + props.template.id}>
+                    Try it   <Image alt='arrow' src="/arrow_white.svg" width={33} height={16} className="w-[20.67px] h-[10.34px] md:w-[33px] md:h-[16px] "></Image>
                 </Link>
-            </div>
+            </div >
 
 
         </>
     );
 };
-export default AiCard;
+export default CenterOfWorldCard;
--- components/landing-page/CenterOfWorldSection.tsx
+++ components/landing-page/CenterOfWorldSection.tsx
@@ -2,10 +2,25 @@
 
 import { FlowTemplate } from '@activepieces/shared';
 import Image from 'next/image'
-import AiCard from './AiCard';
-import { MutableRefObject, useEffect, useRef, useState } from 'react';
-import { Menu, MenuItem, MenuButton, ControlledMenu } from '@szhsin/react-menu';
 
+import { MutableRefObject, useEffect, useRef, useState } from 'react';
+import { MenuItem, ControlledMenu, useClick } from '@szhsin/react-menu';
+import CenterOfWorldCard from './CenterOfWorldCard';
+const apps: { appName: string, icon: string }[] = [
+    {
+        appName: 'Slack',
+        icon: 'https://cdn.activepieces.com/pieces/slack.png'
+    },
+    {
+        appName: 'Asana',
+        icon: 'https://cdn.activepieces.com/pieces/asana.png'
+    },
+    {
+        appName: 'Discord',
+        icon: 'https://cdn.activepieces.com/pieces/discord.png'
+    },
+
+];
 const menuItemClassName = ({ hover }: { hover: boolean }) =>
     hover ? 'apps-menuitem' : 'apps-menuitem';
 
@@ -16,8 +31,11 @@ const CenterOfWorldSection = (props: {
 
     const sectionDiv: MutableRefObject<null | HTMLDivElement> = useRef(null);
     const sliderDiv: MutableRefObject<null | HTMLDivElement> = useRef(null);
-    const menuButtonRef = useRef(null);
+    const menuButtonRef: MutableRefObject<null | HTMLDivElement> = useRef(null);
     const [isAppMenuOpen, setisAppMenuOpen] = useState(false);
+    const [selectedApp, setSelectedApp] = useState(apps[0]);
+    const anchorProps = useClick(isAppMenuOpen, setisAppMenuOpen);
+
     useEffect(() => {
         const handleResize = () => { setMarginLeft(sectionDiv.current ? getComputedStyle(sectionDiv.current).marginLeft : '0px'); }
         handleResize();
@@ -34,33 +52,64 @@ const CenterOfWorldSection = (props: {
                         <Image src="/three.svg" alt='one' width={39} height={41} className='w-[30px] h-[24px] lg:w-[43px] lg:h-[34px]'></Image>
                     </div>
 
-                    <div >
-                        Make <div></div>
+                    <div className='flex gap-[10px] lg:gap-[25px] items-center flex-wrap'>
+                        <div>
+                            Make
+                        </div>
+                        <div className='select-none cursor-pointer  relative items-center select-none flex gap-[8.5px]' {...anchorProps} ref={menuButtonRef} style={
+                            { color: isAppMenuOpen ? "#6E41E2" : "#111111" }
+                        }>
+
+                            <Image alt={selectedApp.appName} src={selectedApp.icon} width={20} height={20} className="h-[26.67px] w-[26.67px] lg:h-[44px] lg:w-[44px]"></Image>
+                            {selectedApp.appName}
+                            <svg width="34" height="18" viewBox="0 0 34 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <g filter="url(#filter0_d_982_1746)">
+                                    <path d="M26.5627 0.280884L17.0002 9.82255L7.43774 0.280884L4.50024 3.21838L17.0002 15.7184L29.5002 3.21838L26.5627 0.280884Z" fill={isAppMenuOpen ? "#6E41E2" : "#111111"} />
+                                </g>
+                                <defs>
+                                    <filter id="filter0_d_982_1746" x="0.500244" y="0.280884" width="33" height="23.4375" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+                                        <feFlood floodOpacity="0" result="BackgroundImageFix" />
+                                        <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
+                                        <feOffset dy="4" />
+                                        <feGaussianBlur stdDeviation="2" />
+                                        <feComposite in2="hardAlpha" operator="out" />
+                                        <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.05 0" />
+                                        <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_982_1746" />
+                                        <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_982_1746" result="shape" />
+                                    </filter>
+                                </defs>
+                            </svg>
+                            <Image alt="underline" src="/menu_underline.svg" width={202} height={193} className='absolute -bottom-[9px] left-[10px]'></Image>
+                        </div>  <div className='basis-full lg:basis-auto'>
+                            the center of your world
+                        </div>
+                        <ControlledMenu
+                            state={isAppMenuOpen ? 'open' : 'closed'}
+                            anchorRef={menuButtonRef}
+                            onClose={() => setisAppMenuOpen(false)} menuClassName="apps-menu">
+                            {
+                                apps.map((app, idx) =>
+                                    <MenuItem className={menuItemClassName} key={idx} onClick={() => setSelectedApp(app)}>
+                                        <div className='item-container text-h4-sm lg:text-h4-lg cursor-pointer flex gap-2  items-center'>
+                                            <div className=" bg-white flex items-center p-[5px] lg:p-[10px] border border-solid border-outline rounded-lg ">
+                                                <Image alt={selectedApp.appName} src={app.icon} width={20} height={20} className="h-[20px] w-[20px] lg:h-[40px] lg:w-[40px]"></Image>
+                                            </div>  {app.appName}
+                                        </div>
+                                        <div className='border-b border-solid border-white  border-opacity-20 mx-[15px]'></div>
+                                    </MenuItem>
+                                )
+                            }
+
+
+                        </ControlledMenu>
+
                     </div>
-                    <ControlledMenu menuClassName="apps-menu">
-                        <MenuItem className={menuItemClassName}>
-                            <div className='item-container'>
-                                New File
-                            </div>
-                            <div className='border-b border-solid border-white  border-opacity-20 mx-[30px]'></div>
-                        </MenuItem>
 
-                        <MenuItem className={menuItemClassName} >
-                            <div className='item-container'>
-                                New File
-                            </div>
-
-                            <div className='border-b border-solid border-white border-opacity-20  mx-[30px]'></div></MenuItem>
-
-                        <MenuItem className={menuItemClassName}>  <div className='item-container'>
-                            New File
-                        </div></MenuItem>
-                    </ControlledMenu>
 
                 </div>
                 <div className='flex'>
                     <h2 className='text-black text-h6-sm lg:text-h6-lg mt-[20px] lg:mt-[40px] max-w-[958px] mb-[40px] lg:mb-[80px]'>
-                        Grow your team of AIs to scale up your blog content, offer customer service and auto post to your social accounts.
+                        Align your team by spreading the news where they hang out, send notifications from everywhere to your Slack workspace.
                     </h2>
                     <div className='hidden lg:flex gap-[60px] grow justify-end'>
                         <Image alt='arrow' src="/arrow.svg" width={33} height={16} className='rotate-180 cursor-pointer'
@@ -84,7 +133,7 @@ const CenterOfWorldSection = (props: {
                 }
             } className='flex gap-[15px] overflow-scroll px-4 lg:px-0 ai-cards-slider items-center' ref={sliderDiv}>
                 {
-                    props.props.map((_, i) => <AiCard key={i} template={_.template} piecesIcons={_.piecesIcons}></AiCard>)
+                    props.props.map((_, i) => <CenterOfWorldCard bannerUrl={'/random_' + ((i % 3) + 1) + '.svg'} key={i} template={_.template} piecesIcons={_.piecesIcons}></CenterOfWorldCard>)
                 }
 
             </div>
