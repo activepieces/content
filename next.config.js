a9aecd99778b0777caa1df9c96571e46e17d8892
Mohammad AbuAboud <moabuaboud@gmail.com>
2023-08-10 23:57:01 +0200

    fix: docs rewrite

--- next.config.js
+++ next.config.js
@@ -1,18 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'standalone',
+    async rewrites() {
+      return [
+        {
+          source: "/docs/:path*",
+          destination: "https://activepieces.mintlify.dev/docs/:path*",
+        },
+      ];
+    },
     async redirects() {
         return [
           {
             source: '/plans',
             destination: '/pricing',
             permanent: true,
-          },
-          {
-            source: '/docs',
-            destination: 'https://activepieces.mintlify.dev/docs/getting-started/introduction',
-            permanent: true,
-          },
+          }
         ]
       },
     images: {
