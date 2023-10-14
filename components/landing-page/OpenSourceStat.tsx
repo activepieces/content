cdfefd674cb20148d1e2d00614f61f31bff357a5
Abdullah Abu Sham <abusham_93@hotmail.com>
2023-10-14 21:37:35 +0300

    -Landing page activepieces is open source section arrows gap

--- components/landing-page/OpenSourceStat.tsx
+++ components/landing-page/OpenSourceStat.tsx
@@ -13,19 +13,16 @@ const OpenSourceStat = (stat: OpenSourceStatProps) => {
     return (
         <>
             <Link href={stat.ctaUrl} target="_blank" rel="noopener noreferer" className='flex fill-black hover:fill-primary hover:text-primary text-black transition-colors flex-wrap md:flex-nowrap items-center'>
-                <div className='flex gap-[15px] items-center basis-full md:basis-0  gap-[25px] grow text-h4-sm md:text-h4-lg'>
+                <div className='flex gap-[15px] items-center basis-full md:basis-0 gap-[25px] grow text-h4-sm md:text-h4-lg'>
                     {stat.logo}
                     {stat.statText}
                 </div>
 
-                <div className="flex items-center gap-[5px] md:px-[5px] mt-[24px] lg:mt-[0px] font-bold lg:font-medium text-h6-sm md:text-h6-lg">
+                <div className="flex items-center gap-[10px] md:px-[5px] mt-[24px] lg:mt-[0px] font-bold lg:font-medium text-h6-sm md:text-h6-lg">
                     {stat.ctaText} <Arrow></Arrow>
 
                 </div>
             </Link>
-
-
-
         </>
     );
 };
