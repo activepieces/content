fd4cebacc14bca26c815760fcbf07378941e5dd4
Abdullah Abu Sham <abusham_93@hotmail.com>
2023-10-21 20:46:03 +0300

    -Fully fixed 'Activepieces is open source' section responsive issues, plus cleanup

--- components/landing-page/OpenSourceStat.tsx
+++ components/landing-page/OpenSourceStat.tsx
@@ -12,15 +12,13 @@ export interface OpenSourceStatProps {
 const OpenSourceStat = (stat: OpenSourceStatProps) => {
     return (
         <>
-            <Link href={stat.ctaUrl} target="_blank" rel="noopener noreferer" className='flex fill-black hover:fill-primary hover:text-primary text-black transition-colors flex-wrap md:flex-nowrap items-center'>
-                <div className='flex gap-[15px] items-center basis-full md:basis-0 gap-[25px] grow text-h4-sm md:text-h4-lg'>
+            <Link href={stat.ctaUrl} target="_blank" rel="noopener noreferer" className="flex fill-black hover:fill-primary hover:text-primary text-black transition-colors flex-wrap md:flex-nowrap items-center max-[1319px]:gap-y-4 max-[1319px]:flex-col max-[1023px]:gap-y-0 max-[1023px]:flex-row">
+                <div className="flex gap-[15px] items-center basis-full md:basis-0 gap-[25px] grow text-h4-sm md:text-h4-lg">
                     {stat.logo}
                     {stat.statText}
                 </div>
-
-                <div className="flex items-center gap-[10px] md:px-[5px] mt-[24px] lg:mt-[0px] font-bold lg:font-medium text-h6-sm md:text-h6-lg">
+                <div className="flex items-center gap-[10px] md:px-[5px] mt-[24px] md:mt-[0px] font-bold lg:font-medium text-h6-sm md:text-h6-lg">
                     {stat.ctaText} <Arrow></Arrow>
-
                 </div>
             </Link>
         </>
